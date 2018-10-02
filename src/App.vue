9db0470504340829b46da5128f03c3459d0c9917
Dody Setiyawan <dody.setiyawan11@gmail.com>
2018-10-02 17:01:36 +0700

    first repo for simple game using vuejs

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,108 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <sui-grid :columns="3">
+      <sui-grid-row class="textYouAndMonster">
+        <sui-grid-column class="paddingLeft">
+          <h1>YOU</h1>
+          <div class="marginLeft">
+            <sui-progress
+              color="green"
+              :percent="playerHealth"
+              progress
+            />
+          </div>
+        </sui-grid-column>
+        <sui-grid-column class="paddingRight">
+          <h1>MONSTER</h1>
+          <div class="marginRight">
+            <sui-progress    
+              color="green"
+              :percent="monsterHealth"
+              style="text-align:center;"
+              progress
+            />
+          </div>
+        </sui-grid-column>
+      </sui-grid-row>
+    </sui-grid>
+    <div class="paddingSegment">
+      <sui-segment>
+        <section v-if="!gameIsRunning">
+          <sui-button class="buttonStart" @click="startGame">
+            START NEW GAME
+          </sui-button>
+        </section>
+        <section v-else>
+          <sui-button color="green" inverted @click="attack">
+            ATTACK
+          </sui-button> 
+          <sui-button color="red" inverted @click="specialAttack">
+            SPECIAL ATTACK
+          </sui-button>
+          <sui-button :style="{button: 'disabled'}" color="blue" inverted @click="heal">
+            HEAL
+          </sui-button>
+          <sui-button color="yellow" inverted @click="giveUp">
+            GIVE UP
+          </sui-button>
+        </section>
+      </sui-segment>
+      <sui-segment v-if="turns.length > 0">
+        <th>
+          <p v-for="(turn, index) in turns" :key="index">
+            {{ turn.text }}
+          </p>
+        </th>
+      </sui-segment>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      gameIsRunning: false,
+      textAlign: 'left',
+      buttonVisibleHeal: 'button',
+      turns: []
+    }
+  },
+  methods: {
+    
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  /* font-family: 'Avenir', Helvetica, Arial, sans-serif; */
+  /* -webkit-font-smoothing: antialiased; */
+  /* -moz-osx-font-smoothing: grayscale; */
   text-align: center;
-  color: #2c3e50;
+  /* color: #2c3e50; */
   margin-top: 60px;
+}
+
+.buttonStart {
+  background-color:#35bdb2 !important;
+  color:#fff !important;
+}
+
+.paddingLeft {
+  margin-left: 12%;
+}
+
+.paddingRight {
+  margin-left: 9%;
+}
+
+.paddingSegment {
+  padding-left: 10%;
+  padding-right: 10%;
 }
 </style>
